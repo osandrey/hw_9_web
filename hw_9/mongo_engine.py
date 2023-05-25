63b0fa395ea4c1b249a463f903e6dc41b05d56c6
osandrey <osandreyman@gmail.com>
2023-05-25 18:51:17 +0100

    New update

--- hw_9/mongo_engine.py
+++ hw_9/mongo_engine.py
@@ -9,17 +9,17 @@ connection = connect(host=uri,  tlsCAFile=certifi.where(), ssl=True)
 
 class Author(Document):
 
-    fullname = StringField(max_length=50)
-    born_date = StringField(max_length=50)
+    fullname = StringField(max_length=100)
+    born_date = StringField(max_length=100)
     # born_date = DateTimeField()
-    born_location = StringField(max_length=50)
+    born_location = StringField(max_length=1200)
     description = StringField()
 
 
 class Quote(Document):
 
     author = ReferenceField(Author, reverse_delete_rule=CASCADE)
-    tags = ListField(StringField(max_length=30))
+    tags = ListField(StringField(max_length=100))
     quote = StringField(max_length=1200, required=True)
 
     meta = {'allow_inheritance': True}
